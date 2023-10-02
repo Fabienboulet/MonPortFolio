35568ba9396f238fe50ba646b660fe24156ec0be
Fabien Boulet <fabien.boulet@gmail.com>
2023-10-02 20:42:14 +0200

    test

--- src/info/Info.js
+++ src/info/Info.js
@@ -95,31 +95,31 @@ export const info = {
     portfolio: [ // This is where your portfolio projects will be detailed
         {
             title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            live: "", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
             source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
             title: "Project 2",
-            live: "https://paytonpierce.dev",
+            live: "",
             source: "https://github.com/paytonjewell",
             image: mock2
         },
         {
             title: "Project 3",
-            live: "https://paytonpierce.dev",
+            live: "",
             source: "https://github.com/paytonjewell",
             image: mock3
         },
         {
             title: "Project 4",
-            live: "https://paytonpierce.dev",
+            live: "",
             source: "https://github.com/paytonjewell",
             image: mock4
         },
         {
             title: "Project 5",
-            live: "https://paytonpierce.dev",
+            live: "",
             source: "https://github.com/paytonjewell",
             image: mock5
         }
